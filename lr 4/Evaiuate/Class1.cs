fb74a49165623cc59201812c05618665cb7cf758
katpopova2000 <katpopova2000@mail.ru>
2021-11-19 22:41:19 +0300

    1

---
+++ lr 4/Evaiuate/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaiuate
+{
+    public static class Evaiuate
+    {
+        public static double del (double number1, double number2)
+        {
+            return number1 / number2;
+
+        }
+        
+    }
+}
